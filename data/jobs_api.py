535b2257c95deabe05dcc772e0d6c28c14cddb81
IDRERD <idrerd999@gmail.com>
2024-03-09 19:18:16 +0400

    SQLMars REST API add_job

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -19,3 +19,24 @@ def get_job(job_id):
     if not job:
         return flask.make_response(flask.jsonify({"error": "Not Found"}), 404)
     return flask.jsonify({"job": job.to_dict()})
+
+
+@blueprint.route("/api/jobs", methods=["POST"])
+def add_job():
+    if not flask.request.json:
+        return flask.make_response(flask.jsonify({"error": "Empty Request"}), 400)
+    elif not all(key in flask.request.json for key in ["team_leader", "job", "work_size", "collaborators", "is_finished"]):
+        return flask.make_response(flask.jsonify({"error": "BadRequest"}), 400)
+    dbs = db_session.create_session()
+    job = Jobs(
+        team_leader=flask.request.json["team_leader"],
+        job=flask.request.json["job"],
+        work_size=flask.request.json["work_size"],
+        collaborators=flask.request.json["collaborators"],
+        # start_date=flask.request.json["start_date"],
+        # end_date=flask.request.json["end_date"],
+        is_finished=flask.request.json["is_finished"]
+    )
+    dbs.add(job)
+    dbs.commit()
+    return flask.jsonify({"id": job.id})
--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from requests import get, post
+import datetime
 
-print(get("http://localhost:5000/api/jobs").json())
-print(get("http://localhost:5000/api/jobs/1").json())
-print(get("http://localhost:5000/api/jobs/11111").json())
-print(get("http://localhost:5000/api/jobs/first_job").json())
+# print(get("http://localhost:5000/api/jobs").json())
+# print(get("http://localhost:5000/api/jobs/1").json())
+# print(get("http://localhost:5000/api/jobs/11111").json())
+# print(get("http://localhost:5000/api/jobs/first_job").json())
+
+print(post("http://localhost:5000/api/jobs", json={
+    'team_leader': 1,
+    'job': 'Test job1',
+    'work_size': 24,
+    'collaborators': '1, 2',
+    # 'start_date': datetime.datetime.now(),
+    # 'end_date': (datetime.datetime.now() + datetime.timedelta(days=1)),
+    'is_finished': False
+}).json())
