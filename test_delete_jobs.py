09cd7db7ff26b3ba2987a50991c7ab5afa1a2d0e
IDRERD <idrerd999@gmail.com>
2024-03-10 11:37:34 +0400

    SQL Mars REST API test delete jobs

--- test_delete_jobs.py
+++ test_delete_jobs.py
@@ -1,3 +1,11 @@
-from requests import delete
+from requests import delete, get
 
-print(delete("http://localhost:5000/api/jobs/4").json())
+print(get("http://localhost:5000/api/jobs").json())  # Jobs before test requests
+print("----------------------------------")
+
+print(delete("http://localhost:5000/api/jobs/3").json())  # Correct request
+print(delete("http://localhost:5000/api/jobs/first_job").json())  # job_id can't be str type
+print(delete("http://localhost:5000/api/jobs/1111111").json())  # No job with id 1111111 in database
+
+print("----------------------------------")
+print(get("http://localhost:5000/api/jobs").json())  # Jobs after test requests
