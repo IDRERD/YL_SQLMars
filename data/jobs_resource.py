fbebf2af11fed44de580e84ab65ffb9c7e6f32df
IDRERD <idrerd999@gmail.com>
2024-03-11 18:04:39 +0400

    SQL Mars RESTful API bugfix

--- data/jobs_resource.py
+++ data/jobs_resource.py
@@ -3,16 +3,14 @@ import flask_restful as fr
 import flask_restful.reqparse as reqparse
 
 from data import db_session
-from data.users import User
+from data.jobs import Jobs
 
 parser = reqparse.RequestParser()
-parser.add_argument('surname', required=True)
-parser.add_argument('name', required=True)
-parser.add_argument('age', required=True, type=int)
-parser.add_argument('position', required=True)
-parser.add_argument('speciality', required=True)
-parser.add_argument('address', required=True)
-parser.add_argument('email', required=True)
+parser.add_argument('title', required=True)
+parser.add_argument('content', required=True)
+parser.add_argument('is_private', required=True, type=bool)
+parser.add_argument('is_published', required=True, type=bool)
+parser.add_argument('user_id', required=True, type=int)
 
 
 def abort_if_not_found(user_id):
